38ae383664d404f9b10c0f712fc26128192643b4
Lama El-Sherbiny <lamasherb@gmail.com>
2021-08-16 11:05:53 -0400

    user-seeding

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,14 @@ from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+followers = db.Table(
+    "followers",
+    db.Model.metadata,
+    db.Column("followerId", db.Integer, db.ForeignKey("users.id")),
+    db.Column("followingId", db.Integer, db.ForeignKey("users.id")),
+    db.Column("timestamp", db.DateTime, default=datetime.now)
+)
+
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -9,6 +17,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    bio = db.Column(db.String(255))
+    profile_pic = db.Column(db.String(255))
     hashed_password = db.Column(db.String(255), nullable=False)
 
     @property
@@ -26,5 +36,7 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            "bio": self.bio,
+            "profile_pic": self.profile_pic
         }
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -9,10 +9,72 @@ def seed_users():
         username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
         username='bobbie', email='bobbie@aa.io', password='password')
+    user1 = User(
+        username='streetarts', email='streetarts@aa.io',
+                      password='password', profile_pic='', bio="")
+    user2 = User(
+        username='lemalooma', email='lema@aa.io',
+                      password='password', profile_pic='', bio="")
+    user3 = User(
+         username='diana', email='diana@aa.io',
+                      password='password', profile_pic='', bio="")
+    user4 = User(
+         username='kyle', email='kyle@aa.io',
+                      password='password', profile_pic='', bio="")
+    user5 = User(
+         username='zhuoxin', email='zhuoxin@aa.io',
+                      password='password', profile_pic='', bio="")
+    user6 = User(
+        username='alexandertheartist', email='alexander@aa.io',
+                      password='password', profile_pic='', bio="")
+    user7= User(
+        username='flulemaity', email='flulematiy@aa.io',
+                      password='password', profile_pic='', bio="")
+    user8 = User(
+            username='dolly', email='dolly@aa.io',
+                      password='password', profile_pic='', bio="")
+    user9 = User(
+            username='harry', email='harry@aa.io',
+                      password='password', profile_pic='', bio="")
+    user10 = User(
+        username='mervstheword', email='merv@aa.io',
+                      password='password', profile_pic='', bio="")
+    user11 = User(
+        username='artgod', email='artgod@aa.io',
+                      password='password', profile_pic='', bio="")
+    user12 = User(
+        username='vanDough', email='vadough@aa.io',
+                      password='password', profile_pic='', bio="")
+    user13 = User(
+        username='callen_shaub', email='callen@aa.io',
+                      password='password', profile_pic='', bio="")
+    user14 = User(
+        username='oil_paintings', email='oil@aa.io',
+                      password='password', profile_pic='', bio="")
+    user15 = User(
+        username='marvelousmonet', email='monet@aa.io',
+                      password='password', profile_pic='', bio="")
+
 
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.add(user3)
+    db.session.add(user4)
+    db.session.add(user5)
+    db.session.add(user6)
+    db.session.add(user7)
+    db.session.add(user8)
+    db.session.add(user9)
+    db.session.add(user10)
+    db.session.add(user11)
+    db.session.add(user12)
+    db.session.add(user13)
+    db.session.add(user14)
+    db.session.add(user15)
+   
 
     db.session.commit()
 
