347c373b6b02ea93b6f73f9cdd3fef6def92bb2c
Lama El-Sherbiny <lamasherb@gmail.com>
2021-08-18 12:22:58 -0400

    added tan's styling

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -7,51 +7,54 @@ import "./Navbar.css";
 
 const NavBar = ({ userdata, setAuthenticated }) => {
   const user = useSelector(state => state.session.user)
-
-  const history = useHistory();
+  // const sessionUser = useSelector((state) => state.session.user);
+  // const history = useHistory();
   const artygram = "https://i.imgur.com/t3Mtt7E.png";
   const addpic = "https://i.imgur.com/3yiJpcr.png";
+ 
 
-  const postButton = (e) => {
-    e.preventDefault();
-    history.push("/posts/new");
-  };
+ 
+  // const settingsButton = (e) => {
+  //   e.preventDefault();
+  //   history.push(`/users/${userdata.username}/edit`);
+  // };
 
-  const settingsButton = (e) => {
-    e.preventDefault();
-    history.push(`/users/${userdata.username}/edit`);
-  };
-
-  const profileButton = (e) => {
-    e.preventDefault();
-    history.push(`/users/${userdata.username}`);
-  };
+  // const profileButton = (e) => {
+  //   e.preventDefault();
+  //   history.push(`/users/${userdata.username}`);
+  // };
 
 
 
   return (
-    <div className='navbar__container'>
-
-      <div className='logo__container'>
+    <div className="navbar__container">
+      <div className="logo__container">
         <NavLink exact to="/">
-          <img alt={`artygram`} className="artygram_logo" src={`${artygram}`}></img>
+          <img
+            alt={`artygram`}
+            className="artygram_logo"
+            src={`${artygram}`}
+          ></img>
         </NavLink>
       </div>
-      <div className='search__container'>
+      <div className="search__container">
         <input className="search" type="text" placeholder=" Search " />
       </div>
-      <div className='right-navbar'>
-        <div className='addpost__container'>
+      <div className="right-navbar">
+        <div className="addpost__container">
           <NavLink exact to="/post/new">
             <img alt={`addpic`} className="addpic" src={`${addpic}`}></img>
           </NavLink>
         </div>
-        {user ?
-          <div className='logout__container'>
+        {user ? (
+          <div className="logout__container">
             <LogoutButton setAuthenticated={setAuthenticated} />
           </div>
-          : ''}
+        ) : (
+          ""
+        )}
       </div>
+      
     </div>
   );
 };
--- react-app/src/store/post.js
+++ react-app/src/store/post.js
@@ -3,6 +3,10 @@ const GET_POSTS = 'posts/GET_POSTS'
 const DELETE_POST = 'post/DELETE_POST'
 const CREATE_POST = 'posts/CREATE_POST'
 const EDIT_CAPTION = 'posts/EDIT_CAPTION'
+// const LIKE_POST= "posts/LIKE_POST";
+// const UNLIKE_POST = "events/UNLIKE_POST";
+
+
 
 const getPosts = (posts) => ({
     type: GET_POSTS,
@@ -23,6 +27,11 @@ const editCaption = (caption) => ({
     type: EDIT_CAPTION,
     caption
 })
+
+// const likePost = (liked) => ({
+//   type: LIKE_POST,
+//   liked,
+// });
 
 export const getAllPosts = () => async dispatch => {
     const req = await fetch(`/api/posts/`);
@@ -87,26 +96,44 @@ export const createPost = (caption, pic_url) => async dispatch => {
 
 
 
+// export const likeOnePost = () => async dispatch => {
+//   const response = await fetch(`/api/posts/${id}`);
+
+//   if (response.ok) {
+//     const liked = await response.json();
+//     dispatch(likeOnePost(id));
+//   }
+// };
+
+
+
+
+
 const initialState = {};
 
 export default function posts(state = initialState, action) {
     switch (action.type) {
-        case GET_POSTS: {
+      case GET_POSTS: {
+        return { ...state, ...action.posts };
+      }
+      case DELETE_POST: {
+        let afterState = { ...state };
 
-            return { ...state, ...action.posts }
-        }
-        case DELETE_POST: {
-            let afterState = { ...state }
+        delete afterState[action.id];
+        return afterState;
+      }
+      case CREATE_POST: {
+        const newState = { ...state };
+        return newState;
+      }
+      default:
+        return state;
 
-            delete afterState[action.id]
-            return afterState
-
-        }
-        case CREATE_POST: {
-            const newState = { ...state }
-            return newState
-        }
-        default:
-            return state;
+    //   case LIKE_POST: {
+    //     newState = { ...state };
+    //     const newLikes = [...newState.liked, action.post];
+    //     newState.liked = newLikes;
+    //     return newState;
+    //   }
     }
 }
