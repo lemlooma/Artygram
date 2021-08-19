a8804d4e59a46011da894d18fb71f658229f0a76
Lama El-Sherbiny <lamasherb@gmail.com>
2021-08-19 09:43:00 -0400

    css

--- react-app/src/components/PostForm/index.js
+++ react-app/src/components/PostForm/index.js
@@ -31,40 +31,51 @@ const PostForm = () => {
     }
 
     return (
-        <div className="post-form__container">
-            <h1 className='form-title'>Create Post</h1>
-            <form onSubmit={handleSubmit} className='post-form'>
-                <div>{errors.map(error => <li>{error}</li>)}</div>
-                <div className='input__container'>
-                    {/* <label htmlFor='pic_url'>Picture Url</label> */}
-                    <input type='text'
-                        className='input'
-                        name='pic_url'
-                        placeholder='Picture Url'
-                        onChange={(e) => setPic_Url(e.target.value)}
-                        value={pic_url}
-                        required
-                    >
-                    </input>
-                </div>
-                <div className='input__container'>
-                    {/* <label htmlFor='caption'>Caption</label> */}
-                    <textarea type='text'
-                        name='caption'
-                        className='textarea'
-                        placeholder='Caption here!!'
-                        onChange={(e) => setCaption(e.target.value)}
-                        value={caption}
-                        required
-                    >
-                    </textarea>
-                </div>
-                <button className='create-button' type='submit'>Create</button>
-            </form>
+      <div className="post-form__container">
+        <i className="fab fa-instagram fa-2x"></i>
+        <h1 className="form-title">Upload Post</h1>
+        <form onSubmit={handleSubmit} className="post-form">
+          <div>
+            {errors.map((error) => (
+              <li>{error}</li>
+            ))}
+          </div>
+          <div className="input__container">
+            {/* <label htmlFor='pic_url'>Picture Url</label> */}
+            <input
+              type="text"
+              className="input"
+              name="pic_url"
+              placeholder="Picture Url"
+              onChange={(e) => setPic_Url(e.target.value)}
+              value={pic_url}
+              required
+            ></input>
+          </div>
+          <div className="input__container">
+            {/* <label htmlFor='caption'>Caption</label> */}
+            <textarea
+              type="text"
+              name="caption"
+              className="textarea"
+              placeholder="Caption here!!"
+              onChange={(e) => setCaption(e.target.value)}
+              value={caption}
+              required
+            ></textarea>
+          </div>
+          <button className="create-button" type="submit">
+            Create
+          </button>
+        </form>
 
-            <div><button className='cancel-button' onClick={cancel}>Cancel</button></div>
+        <div>
+          <button className="cancel-button" onClick={cancel}>
+            Cancel
+          </button>
         </div>
-    )
+      </div>
+    );
 }
 
 export default PostForm;
