6fca0c04b73e99387b4e4cc7522e65cbca00eec2
kyle <ktseng122@gmail.com>
2021-08-20 17:52:19 -0400

    fixed double username and icons on homepage

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -6,71 +6,66 @@ import './homepage.css'
 
 
 const HomePage = () => {
-    const user = useSelector(state => state.session.user)
-    const posts = useSelector(state => Object.values(state.posts))
-    const dispatch = useDispatch();
+  const user = useSelector(state => state.session.user)
+  const posts = useSelector(state => Object.values(state.posts))
+  const dispatch = useDispatch();
 
-    const [getPost, setPost] = useState(false);
+  const [getPost, setPost] = useState(false);
 
-    const Ids = user.follows.map(user => user.id)
-    Ids.push(user.id)
-    const filtered = posts.filter((post) => Ids.includes(post.user_id))
+  const Ids = user.follows.map(user => user.id)
+  Ids.push(user.id)
+  const filtered = posts.filter((post) => Ids.includes(post.user_id))
 
-    const sortedPosts = filtered.reverse()
-
-
-    useEffect(() => {
-        dispatch(getAllPosts())
-    }, [])
-
-    useEffect(() => {
-        if(getPost){
-            dispatch(likeOnePost(getPost))
-            // dispatch(getAllPosts())
-        }
-    }, [getPost])
+  const sortedPosts = filtered.reverse()
 
 
+  useEffect(() => {
+    dispatch(getAllPosts())
+  }, [])
+
+  useEffect(() => {
+    if (getPost) {
+      dispatch(likeOnePost(getPost))
+      // dispatch(getAllPosts())
+    }
+  }, [getPost])
 
 
 
-    return (
-        <div className='photo-feed__container'>
-            {sortedPosts?.map(post =>
-            (<div key={post.id} className='single-post__container'>
-                <div className='icon-username__container'>
 
-                    <Link to={`/user/${post.user_id}`}>
-                    <img className='post-icon' id='post-icon' src={`${post.user?.profile_pic}`} />
-                    </Link>
-                    <Link className='post-username'to={`/user/${post.user_id}`}>
-                    <span> {post.user?.username}</span>
-                    </Link>
 
-                    <img className='post-icon' id='post-icon' src={`${post.user?.profile_pic}`} />
-                    <span className='post-username'> {post.user?.username}</span>
-
-                </div>
-                <div>
-                    <Link to={`post/${post.id}`}>
-
-                        <img className='post-img' src={post.pic_url} alt={`img-${post.id}`} />
-                    </Link>
-                </div>
-                <div>
-                    {post.postlikes.includes(user.id)
-                    ? <button className='likebutton' onClick={() => setPost(post)} ><i class="fas liked fa-heart"></i></button>
-                     :<button className='likebutton' onClick={() => setPost(post)} ><i className="far unliked fa-heart"></i></button>}
-
-                </div>
-                <div>likes: {post.likesnum}</div>
-                <div>{post.caption}</div>
-                <div>comments: {post.commentsnum}</div>
-                <div>{post.timestamp}</div>
-            </div>)
-            )}
+  return (
+    <div className='photo-feed__container'>
+      {sortedPosts?.map(post =>
+      (<div key={post.id} className='single-post__container'>
+        <div className='icon-username__container'>
+          <Link to={`/user/${post.user_id}`}>
+            <img className='post-icon' id='post-icon' src={`${post.user?.profile_pic}`} />
+          </Link>
+          <Link className='post-username' to={`/user/${post.user_id}`}>
+            <span> {post.user?.username}</span>
+          </Link>
         </div>
-    )
+        <div>
+          <Link to={`post/${post.id}`}>
+
+            <img className='post-img' src={post.pic_url} alt={`img-${post.id}`} />
+          </Link>
+        </div>
+        <div>
+          {post.postlikes.includes(user.id)
+            ? <button className='likebutton' onClick={() => setPost(post)} ><i class="fas liked fa-heart"></i></button>
+            : <button className='likebutton' onClick={() => setPost(post)} ><i className="far unliked fa-heart"></i></button>}
+
+        </div>
+        <div>likes: {post.likesnum}</div>
+        <div>{post.caption}</div>
+        <div>comments: {post.commentsnum}</div>
+        <div>{post.timestamp}</div>
+      </div>)
+      )}
+    </div>
+  )
 }
 
 export default HomePage
