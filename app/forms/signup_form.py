3c23669defd1bd827bb7b22f834f3371252928ff
Lama El-Sherbiny <lamasherb@gmail.com>
2021-09-24 12:17:15 -0400

    error handling - sign up / login)

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -20,8 +20,31 @@ def username_exists(form, field):
         raise ValidationError('Username is already in use.')
 
 
+def email_valid(form, field):
+
+    email = field.data
+
+    if '@' not in email:
+        raise ValidationError('Email must have @ symbol')
+
+
+def username_length(form, field):
+    # Check if username length is less than 25 chars
+    username = field.data
+    if len(username) > 15:
+        raise ValidationError('Username must be less 15 than  characters.')
+
+
+def email_length(form, field):
+    # Check if email is less than 30 characters
+    email = field.data
+    if len(email) > 80:
+        raise ValidationError(
+            'Email must be less than 80 characters.')
+
+
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+        'username', validators=[DataRequired(), username_exists, username_length])
+    email = StringField('email', validators=[DataRequired(), user_exists, email_length, email_valid])
     password = StringField('password', validators=[DataRequired()])
